00a8f874ce09a2232b4ca3f34f8ce907e4392b04
Tech_Normy <125196993+Farhans-Lair@users.noreply.github.com>
2025-12-21 23:10:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_saznWqYeR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6mi5licaiqcfd0m26s0pervtvp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_1Xg5FvtgB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '59g8qms0pn0jj7ank3s7jven6g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://jt47ui42k7.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
