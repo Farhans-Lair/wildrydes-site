cbe28cf5859ef5b766fef64c9cb88c6391d8c956
Tech_Normy <125196993+Farhans-Lair@users.noreply.github.com>
2026-01-15 19:49:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_1Xg5FvtgB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '59g8qms0pn0jj7ank3s7jven6g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_kuFJLYAQP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5pdp27krk8fvkhgi7t15vnlbhe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
